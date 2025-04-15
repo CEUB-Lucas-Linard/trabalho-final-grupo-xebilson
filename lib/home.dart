25b2d3f2e254c836998adca61e42368106d31eb3
madmattp <mpqfreitas@gmail.com>
2025-04-15 18:12:05 -0300

    Added ListView.builder Widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xebilson/contacts.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -11,6 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List contacts = [
+    Contact(name: 'Caio', phoneNumber: '(61) 94002-8922'),
+    Contact(name: 'Matheus', phoneNumber: '(61) 91111-1111'),
+    Contact(name: 'Rafael', phoneNumber: '(61) 92222-2222'),
+    Contact(name: 'Kayke', phoneNumber: '(61) 93333-3333')
+    ];
 
   // TODO
   void _add_contact() {
@@ -27,7 +35,24 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Placeholder(),
+      body: Row(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: contacts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          onTap: (){},
+
+                          leading: CircleAvatar(child: Icon(Icons.person)),
+                          title: Text(contacts[index].name),
+                          subtitle: Text(contacts[index].phoneNumber),
+                          trailing: Icon(Icons.more_vert),
+                      );
+                  }
+                  ))
+        ]
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _add_contact,
         tooltip: 'Adicionar Contato',
